92641235d495dd7084b50b07b4cc13caccf75e46
BalckMix <mixchatmix3@gmail.com>
2017-12-02 14:30:17 -0300

    Some crazy

---
+++ src/bootstrap/command.js
@@ -0,0 +1,29 @@
+export const command = crazy => {
+  const { io, rl, users, debbug } = crazy
+    rl.on('line', (line) => {
+      rl.prompt()
+      // let data = {}
+      if(line.indexOf('/users') == 0) {
+        debbug('All users: ' + JSON.stringify(users))
+      }
+
+      if(line.indexOf('/updateList') == 0) {
+        users.length = 0
+        debbug('Update sent.')
+        setTimeout(() => { io.emit('updateUsersList', true) }, 1000)
+      }
+
+      if(line.indexOf('/selectUser') == 0) {
+        let select = line.substring(11)
+        let user = users.filter(user => user.id == select)
+        debbug(user)
+      }
+
+      if(line.indexOf('/debug') == 0) {
+        debbug('Mode Debug: ' + Debug)
+      }
+    }).on('close', () => {
+      debbug('stop Shutting down')
+      process.exit(0)
+  })
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import {io, start, redis, readline, moment, st, log, debbug, redispass, pront, Debug} from './bootstrap'
 import { watch } from './bootstrap/socketWatch'
+import { command } from './bootstrap/command'
 import { setTimeout } from 'timers'
 
 start()
@@ -12,42 +13,11 @@ let bans = []
 watch(io, redis, numUsers, users, st, redispass, debbug)
 
 if (pront.use) {
-  var rl = readline.createInterface(process.stdin, process.stdout)
+  let rl = readline.createInterface(
+    process.stdin,
+    process.stdout
+  )
   rl.setPrompt(pront.prompt)
   rl.prompt()
-}
-  /* Internal */
-  if(pront.use) {
-    readLine()
-  }
-  
-function readLine () {
-  rl.on('line', (line) => {
-    rl.prompt()
-
-      // let data = {}
-
-      if(line.indexOf('/users') == 0) {
-        debbug('All users: ' + JSON.stringify(users))
-      }
-
-      if(line.indexOf('/updateList') == 0) {
-        users.length = 0
-        debbug('Update sent.')
-        setTimeout(() => { io.emit('updateUsersList', true) }, 1000)
-      }
-
-      if(line.indexOf('/selectUser') == 0) {
-        let select = line.substring(11)
-        let user = users.filter(user => user.id == select)
-        debbug(user)
-      }
-
-      if(line.indexOf('/debug') == 0) {
-        debbug('Mode Debug: ' + Debug)
-      }
-    }).on('close', () => {
-      debbug('stop Shutting down')
-      process.exit(0)
-  })
+  command({ io, rl, users, debbug })
 }
